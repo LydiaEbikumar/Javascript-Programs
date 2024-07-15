20360c61481f6de3c3f3edf6b5fe14bc19cf9176
LydiaEbikumar <lydiaebikumar@gmail.com>
2024-07-15 17:32:07 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,80 @@
+//program 1
+// function condition(num){
+//     for(itr=1;itr<=num;itr++)
+//     {
+//     if(itr%3==0 && itr%5!=0)
+//         console.log("fizz");
+//     else if(itr%5==0 && itr%3!=0)
+//         console.log("Bus");
+//     else if(itr%3==0 && itr%5==0)
+//         console.log("Busfizz")
+//     else{
+//         console.log(itr);
+//     }}
+// }
+// condition(100);
+
+//program 2 
+// function text(str){
+//   let str1=(str.split("").reverse().join(""));
+//   if(str==str1){
+//     return("palindrome");}
+//   else{
+//     return("Not a palindrome");
+//   }
+//  }
+// console.log(text("kayak"));
+
+//program 3
+// let arr=[10,30,60,70];
+// arr.sort();
+// console.log(arr[arr.length-1]);
+
+//program 6
+// function factorial(num){
+//     let fact=1,itr;
+//     for(itr=1;itr<=num;itr++)
+//     {
+//         fact=fact*itr;
+//     }
+//     return fact;
+// }
+// console.log(factorial(6));
+
+//program 7
+// function fahrenheit(celsius){
+//       let temp=(celsius-32)*5/9;
+//       return temp;
+//     }
+// console.log(fahrenheit(40));
+
+//program 5
+// let str="this place looks beautiful";
+// let words=str.split(" ");
+// let longest=" ";
+// for(let itr=0;itr<words.length;itr++){
+//     if(words[itr].length>longest.length){
+//         longest=words[itr];
+//     }
+// }
+// console.log(longest); 
+
+//program 4
+// function count(str,char){
+//     let count=0;
+//     for(let itr=0;itr<str.length;itr++){
+//         if(str[itr]==char){
+//             count++
+//         }
+// }
+// return count;
+// }
+// console.log("hello","hi");
+
+
+
+
+
+
+
+
